21546d5836d668cdcb1f7c2731a97d3b983e2f86
Chinmay Garde <chinmaygarde@google.com>
2025-07-28 13:25:56 -0700

    Fixups.

--- app/src/main/cpp/embedder.cc
+++ app/src/main/cpp/embedder.cc
@@ -3,9 +3,10 @@
 #include <jni.h>
 #include <stdlib.h>
 
-__attribute__((visibility("default"))) void
-ANativeActivity_onCreate(ANativeActivity *activity, void *savedState,
-                         size_t savedStateSize) {
+__attribute__((visibility("default"))) void ANativeActivity_onCreate(
+    ANativeActivity* activity,
+    void* savedState,
+    size_t savedStateSize) {
   __android_log_print(ANDROID_LOG_FATAL, "embedder", "Hello2");
   abort();
 }
