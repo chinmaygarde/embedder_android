705aba9f4861de5b90d9a414ff4b7501ac04c05e
Chinmay Garde <chinmaygarde@google.com>
2025-07-28 13:19:26 -0700

    Wire up Clangd.

--- app/src/main/cpp/embedder.cc
+++ app/src/main/cpp/embedder.cc
@@ -1,10 +1,11 @@
-#include <jni.h>
-#include <string>
-#include <android/native_activity.h>
 #include <android/log.h>
+#include <android/native_activity.h>
+#include <jni.h>
+#include <stdlib.h>
 
-__attribute__((visibility("default"))) void ANativeActivity_onCreate(ANativeActivity* activity,
-        void* savedState, size_t savedStateSize) {
-    __android_log_print(ANDROID_LOG_FATAL, "embedder", "Hello2");
-    abort();
+__attribute__((visibility("default"))) void
+ANativeActivity_onCreate(ANativeActivity *activity, void *savedState,
+                         size_t savedStateSize) {
+  __android_log_print(ANDROID_LOG_FATAL, "embedder", "Hello2");
+  abort();
 }
