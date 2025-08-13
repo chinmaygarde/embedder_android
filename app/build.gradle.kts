3f805377d842e705d9e56b590df606ac0c507b13
Chinmay Garde <chinmaygarde@google.com>
2025-08-13 11:37:11 -0700

    Copy assets.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,18 +36,18 @@ android {
         }
     }
 
-
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
         }
     }
+
     buildFeatures {
         viewBinding = true
     }
